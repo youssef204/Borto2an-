ff23b309846285e5b584b4b2e98c58cbcde67652
youssefreda844@gmail.com <youssefreda844@gmail.com>
2021-11-03 16:24:01 +0200

    flight schema finished

--- models/Flight.js
+++ models/Flight.js
@@ -1,1 +1,62 @@
 // bare file to fill later with the schema of the db
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const FlightSchema = new Schema({
+  from: {
+    type: String,
+    required: true,
+  },
+  to: {
+    type: String,
+    required: true
+  },
+  flightNumber: {
+    type: Number,
+    required: true,
+  },
+  flightDate: {
+    type: Date,
+    required: true
+  },
+  departureTime: {
+    type: String,
+    required: false
+  },
+  arrivalTime: {
+    type: String,
+    required: false
+  },
+  seatsAvailable: {
+    type: Number,
+    required: false
+  },
+  cabin: {
+    type: String,
+    required: false
+  },
+  totalSeats: {
+    type: Number,
+    required: false
+  },
+  airplaneType: {
+    type: String,
+    required: false
+  },
+  duration: {
+      type : Number,
+      required : false
+  },
+  airline: {
+    type : String,
+    required : true
+},
+hasTranist: {
+    type : Boolean,
+    required : true
+},
+}, { timestamps: true });
+
+const Flight = mongoose.model('Flight', FlightSchema);
+module.exports = Flight;
