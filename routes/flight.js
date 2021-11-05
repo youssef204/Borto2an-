272c8e5dd93f32bf3cfba2937189aef557950640
youssefreda844@gmail.com <youssefreda844@gmail.com>
2021-11-05 13:31:29 +0200

    get filtered data but didn't test .. problem in connection

--- routes/flight.js
+++ routes/flight.js
@@ -1,52 +1,16 @@
 // routes
 const express = require('express');
 const router = express.Router();
+const flight = require('../models/Flight');
 
-
-// router.get('/' , function(req,res){
-//     mongo.connect(MONGODB_URI,function(err,db) {
-//         var resultArray = [];
-//         var cursor = db.collection("Flight").find(query);
-//         cursor.forEach(function(err,doc) {
-//               resultArray.push(doc);
-//             }, function(){
-//                 db.close();
-//                 res.render('/' , {items:resultArray} )
-//             });
-//     })
-//  }
-// )
 
 
 router.get('/', function (req, res, next) {
-
-    var flight = req.db.get('Flight'),
-        query = req.query;
-
-    // convert year parameter string to int if it exists 
-    if (query.hasOwnProperty("flightNumber")){
-        query["flightNumber"] = parseInt(query.flightNumber);
-    }
-    if (query.hasOwnProperty("totalSeats")){
-        query["totalSeats"] = parseInt(query.totalSeats);
-    }
-    if (query.hasOwnProperty("seatsAvailable")){
-        query["seatsAvailable"] = parseInt(query.seatsAvailable);
-    }
-    if (query.hasOwnProperty("departureTime")){
-        query["departureTime"] = parseInt(query.departureTime);
-    }
-    if (query.hasOwnProperty("arrivalTime")){
-        query["arrivalTime"] = parseInt(query.flightNumber);
-    }
-    if (query.hasOwnProperty("duration")){
-        query["duration"] = parseInt(query.duration);
-    }
-    if (query.hasOwnProperty("flightDate")){
-        query["flightDate"] = parseInt(query.flightDate);    //parse to date
-    }
-    flight.find(query, function (err, docs) {
-        res.json({length: docs.length, records: docs});    
-    });
+   
+   // var flight = req.db.get('Flight'),
+     //   query = req.query;
+    flight.find(req.query)
+    .then(flights => res.json(flights))
+    .catch(err => res.status(404).json({noFlightsFound : "No flights are found"}))
 });
 module.exports = router ;
