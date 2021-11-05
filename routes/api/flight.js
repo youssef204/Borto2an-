0081887cb7d6200a02861b8e4a03dd565a0fba5f
Omar Khair <omarkhair66@gmail.com>
2021-11-05 20:09:09 +0200

    create delete endpoint

--- routes/api/flight.js
+++ routes/api/flight.js
@@ -1,17 +1,39 @@
 // routes
 const express = require("express");
 const flight_router = express.Router();
-const Flights = require("../../models/Flight");
+const Flight = require("../../models/Flight");
 
 //update flight
 flight_router.put("/", (req, res) => {
   //console.log("in update");
   const id = req.body._id;
   const update = req.body.update;
-  Flights.updateOne({ _id: id }, update).then(() => {
+  Flight.updateOne({ _id: id }, update).then(() => {
     console.log("done");
     res.send("done");
   });
+});
+
+//read all flights
+flight_router.get("/showAllflights", (req, res) => {
+  Flight.find()
+    .then((flight) => {
+      res.json(flight);
+    })
+    .catch((err) => res.status(404).json({ msg: "No flights are found" }));
+});
+
+//delete flight with given ID
+flight_router.delete("/:id", async (req, res) => {
+  try {
+    const flight = await Flight.findByIdAndDelete(req.params.id);
+    if (flight) res.send(flight);
+    else
+      res.status(404).json({ msg: `No flight with id ${req.params.id} found` });
+  } catch (e) {
+    console.log(e);
+    res.status(404).json({ msg: `${req.params.id} is not a correct id` });
+  }
 });
 
 module.exports = flight_router;
