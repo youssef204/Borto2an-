7cfb5739dc055cca3a88109a1faebd22a2079ec7
Hesham Moneer <hesham.moneer@outlook.com>
2021-11-05 20:27:34 +0200

    Dev merged with CreateFlight and conflicts resolved

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import CreateFlight from './components/createFlight';
 import Flight from "./components/Flight";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import React from 'react';
 import AllFlights from "./components/allFlights";
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <Router>
         <div>
           <Route exact path='/' component={Flight } />
-          <Route exact path='/All_Flights' component={AllFlights} />
+          <Route exact path='/all_flights' component={AllFlights} />
           <Route exact path='/create_flight' component={CreateFlight} />
         </div>
       </Router> 
