79b945f49ceda62a11cb611049883e3c2252c896
Hesham Moneer <hesham.moneer@outlook.com>
2021-11-05 20:03:14 +0200

    trying

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import CreateFlight from './components/createFlight';
 
 class App extends React.Component {
   render() {
     return (
-      <CreateFlight/>
+	<Router>
+
+		<Route exact path='/create_flight' component={CreateFlight} />
+      		
+	</Router>
     );
   }
 }
