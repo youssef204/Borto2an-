174b51018b0874ce53d5a6c7be920b8f26fee2aa
youssefreda844@gmail.com <youssefreda844@gmail.com>
2021-11-05 16:20:18 +0200

    error 404 page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import Flight from "./components/Flight";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route ,Switch } from 'react-router-dom';
 import React, { Component } from 'react';
-import AllFlights from "./components/AllFlights";
+import AllFlights from "./components/allFlights";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return(
 
     <Router>
         <div>
+          <Switch>
           <Route exact path='/' component={Flight } />
           <Route exact path='/All_Flights' component={AllFlights} />
-
+          <Route component={PageNotFound} />
+          </Switch>
         </div>
       </Router> 
   );
