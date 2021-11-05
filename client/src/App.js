67ad038c63be4f0cfc683de32e4135a805153714
Hesham Moneer <hesham.moneer@outlook.com>
2021-11-05 19:52:41 +0200

    allFlights component was misspelled

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import Flight from "./components/Flight";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React, { Component } from 'react';
-import AllFlights from "./components/AllFlights";
+import AllFlights from "./components/allFlights";
 
 function App() {
   return(
 
     <Router>
         <div>
+	  <Routes>
           <Route exact path='/' component={Flight } />
           <Route exact path='/All_Flights' component={AllFlights} />
-
+	  </Routes>
         </div>
       </Router> 
   );
