432c6fc63dbb4101146db0c69edd499c77661f79
youssefreda844@gmail.com <youssefreda844@gmail.com>
2021-11-06 00:57:33 +0200

    search done with filters > <

--- routes/api/flight.js
+++ routes/api/flight.js
@@ -2,18 +2,21 @@
 const express = require("express");
 const flight_router = express.Router();
 const Flights = require("../../models/Flight");
+var qs = require('qs');
 
 //update flight
 
 
 
 flight_router.get('/', function (req, res, next) {
-   
-   // var flight = req.db.get('Flight'),
-     //   query = req.query;
-    Flights.find(req.query)
+      const queryObj = { ...req.query };
+      let queryStr = JSON.stringify(queryObj)
+      const regex = /\b(gt|gte|lt|lte|in)\b/g;
+      queryStr = queryStr.replace(regex, '$$' + "$1");
+      console.log(JSON.parse(queryStr));
+    Flights.find(JSON.parse(queryStr))
     .then(flight => res.json(flight))
-    .catch(err => res.status(404).json({noFlightsFound : "No flights are found"}))
+    .catch(err => res.status(404).json({msg : "No flights are found"}))
 });
 
 
