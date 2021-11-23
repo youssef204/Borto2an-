5449ac0a2366d0eb930fbb5eef4d20eb22dacd09
PeterFahmi <rlpeterfah@gmail.com>
2021-11-23 22:16:35 +0200

    user model with endpoints for crud

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const validateEmail = (v) => {
+  if (!v.includes("@")) return false;
+  let arr = v.split("@");
+  if (!arr[1].includes(".")) return false;
+  return true;
+};
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: validateEmail,
+      message: `{VALUE} is not in the form of a mail`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
