1d29c5f17a13df2955030e8e725711ee7f65d203
Omar Khair <omarkhair66@gmail.com>
2021-12-01 02:30:00 +0200

    initial link of seat picker to dp

--- client/src/components/SeatSelection.js
+++ client/src/components/SeatSelection.js
@@ -1,10 +1,63 @@
 import React, { Component } from "react";
 import SeatPicker from "react-seat-picker";
+import Button from "@mui/material/Button";
 
 class SeatSelection extends React.Component {
   state = {
     loading: false,
+    selectedSeats: [],
+    prob: {
+      flight:{
+        "_id": "61a59b84b407eba753da9c9a",
+        "flightNumber": 109,
+        "departure": {
+          "airport": "New York",
+          "terminal": "990",
+          "time": "2021-11-30T16:19:00.000Z",
+          "_id": "61a63546e773595397a8261d"
+        },
+        "arrival": {
+          "airport": "Berlin",
+          "terminal": "991",
+          "time": "2021-11-30T11:33:00.000Z",
+          "_id": "61a63546e773595397a8261e"
+        },
+        "airline": "Lufthanza",
+        "hasTransit": true,
+        "airplaneModelID": "61a4efcae90c6cd60e01423f",
+        "economyCabin": {
+          "takenSeats": [],
+          "adultPrice": 100,
+          "adultBaggage": 90,
+          "childPrice": 80,
+          "childBaggage": 70,
+          "_id": "61a636e2bafff62e510ab6ae"
+        },
+        "businessCabin": {
+          "takenSeats": [],
+          "adultPrice": 100,
+          "adultBaggage": 90,
+          "childPrice": 80,
+          "childBaggage": 70,
+          "_id": "61a636e2bafff62e510ab6af"
+        },
+        "firstCabin": {
+          "takenSeats": [],
+          "adultPrice": 100,
+          "adultBaggage": 90,
+          "childPrice": 80,
+          "childBaggage": 70,
+          "_id": "61a636e2bafff62e510ab6b0"
+        },
+        "createdAt": "2021-11-30T03:33:24.048Z",
+        "updatedAt": "2021-11-30T19:22:22.265Z",
+        "__v": 0
+      },
+      numberOfSeats:3,
+      chosenCabin:'economy',
+    }
   };
+  
 
   addSeatCallback = ({ row, number, id }, addCb) => {
     this.setState(
@@ -16,34 +69,10 @@ class SeatSelection extends React.Component {
         console.log(`Added seat ${number}, row ${row}, id ${id}`);
         const newTooltip = `tooltip for id-${id} added by callback`;
         addCb(row, number, id, newTooltip);
-        this.setState({ loading: false });
-      }
-    );
-  };
-
-  addSeatCallbackContinousCase = (
-    { row, number, id },
-    addCb,
-    params,
-    removeCb
-  ) => {
-    this.setState(
-      {
-        loading: true,
-      },
-      async () => {
-        if (removeCb) {
-          await new Promise((resolve) => setTimeout(resolve, 750));
-          console.log(
-            `Removed seat ${params.number}, row ${params.row}, id ${params.id}`
-          );
-          removeCb(params.row, params.number);
-        }
-        await new Promise((resolve) => setTimeout(resolve, 750));
-        console.log(`Added seat ${number}, row ${row}, id ${id}`);
-        const newTooltip = `tooltip for id-${id} added by callback`;
-        addCb(row, number, id, newTooltip);
-        this.setState({ loading: false });
+        const selectedSeats = [...this.state.selectedSeats];
+        selectedSeats.push(id);
+        this.setState({ loading: false, selectedSeats });
+        console.log(this.state.selectedSeats);
       }
     );
   };
@@ -59,12 +88,65 @@ class SeatSelection extends React.Component {
         // A value of null will reset the tooltip to the original while '' will hide the tooltip
         const newTooltip = ["A", "B", "C"].includes(row) ? null : "";
         removeCb(row, number, newTooltip);
-        this.setState({ loading: false });
+        const selectedSeats = this.state.selectedSeats.filter((curID)=>curID != id);
+        this.setState({ loading: false , selectedSeats});
+        console.log(this.state.selectedSeats);
       }
     );
   };
 
+  getSeatsInfo = ()=>{
+    const airplaneModel = this.state.prob.flight.airplaneModelID;
+    const cabin = this.state.prob.chosenCabin;
+    let rows, cols, takenSeats;
+    if(cabin === 'economy'){
+      rows = airplaneModel.economyRows;
+      cols = airplaneModel.economyColumns;
+      takenSeats = this.state.prob.flight.economyCabin.takenSeats;
+    }else if(cabin === 'business'){
+      rows = airplaneModel.businessRows;
+      cols = airplaneModel.businessColumns;
+      takenSeats = this.state.prob.flight.businessCabin.takenSeats;
+    }else if(cabin === 'first'){
+      rows = airplaneModel.firstClassRows;
+      cols = airplaneModel.firstClassColumns;
+      takenSeats = this.state.prob.flight.firstCabin.takenSeats;
+    }
+    return {rows, cols, takenSeats};
+  }
+  buildSeatsArray = ()=>{
+    console.log(this.getSeatsInfo());
+    const {rows, cols, takenSeats} = this.getSeatsInfo();
+    const seats = [];
+    for(let i = 0, id = 1;i<rows;i++){
+      const row = [];
+      for(let j = 0;j<cols;j++, id++){
+        const isReserved = takenSeats.includes(id);
+        const tooltip = (isReserved?"Seat Taken":"Seat Available");
+        const seat = {
+          id: id,
+          number: id,
+          isReserved: isReserved,
+          tooltip:tooltip
+        }
+        row.push(seat);
+      }
+      seats.push(row);
+    }
+    return seats;
+  }
+
+  renderConfirmButton = () => {
+    if(this.state.selectedSeats.length == this.state.prob.numberOfSeats)
+      return (<Button variant="contained">Confirm</Button>);
+    else
+    return (<Button variant="contained" disabled>Confirm</Button>);
+  };
+
+  
   render() {
+    const seats = this.buildSeatsArray();
+    console.log(seats);
     const rows = [
       [
         { id: 1, number: 1, isSelected: true, tooltip: "Reserved by you" },
@@ -136,8 +218,8 @@ class SeatSelection extends React.Component {
           <SeatPicker
             addSeatCallback={this.addSeatCallback}
             removeSeatCallback={this.removeSeatCallback}
-            rows={rows}
-            maxReservableSeats={3}
+            rows={seats}
+            maxReservableSeats={this.state.prob.numberOfSeats}
             alpha
             visible
             selectedByDefault
@@ -145,21 +227,7 @@ class SeatSelection extends React.Component {
             tooltipProps={{ multiline: true }}
           />
         </div>
-        <h1>Seat Picker Continuous Case</h1>
-        <div style={{ marginTop: "100px" }}>
-          <SeatPicker
-            addSeatCallback={this.addSeatCallbackContinousCase}
-            removeSeatCallback={this.removeSeatCallback}
-            rows={rows}
-            maxReservableSeats={3}
-            alpha
-            visible
-            selectedByDefault
-            loading={loading}
-            tooltipProps={{ multiline: true }}
-            continuous
-          />
-        </div>
+        {this.renderConfirmButton()}
       </div>
     );
   }
