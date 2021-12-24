03a497317e06bc92194e4da3bea2f076d86a382d
youssefreda844@gmail.com <youssefreda844@gmail.com>
2021-12-24 19:22:11 +0200

    removed alert from delete model

--- client/src/components/AdminComponents/Model.js
+++ client/src/components/AdminComponents/Model.js
@@ -1,8 +1,21 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 function Flight({ _id,name, economyRows, economyColumns, businessRows, businessColumns, firstClassRows, firstClassColumns, deleteModel}) {
-  
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
   return (
     <>
     <tr>
@@ -14,9 +27,30 @@ function Flight({ _id,name, economyRows, economyColumns, businessRows, businessC
       <td >{firstClassRows}</td>
       <td >{firstClassColumns}</td>
     <td>
-      <button  onClick = {()=>deleteModel(_id)} >
+      <button  onClick = {handleClickOpen} >
         Delete
       </button >
+      <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Are you sure you want to delete this Model ?"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Once delete is clicked this model will be no longer available along side with all flights of that model !
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <button onClick={handleClose}>Cancel</button>
+            <button onClick={()=>deleteModel(_id)} autoFocus>
+              Delete
+            </button>
+          </DialogActions>
+        </Dialog>
       </td>
     </tr>
     </>
